822aa0bece551028a278b13339d53211bbc51299
siegfriedbz <siegfried.bozza@yahoo.com>
2023-09-27 12:06:38 +0200

    change og image

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return (
     <main className='min-h-screen dark:bg-slate-950 dark:text-white'>
-      <RootLayout socialUmageUrl='https://www.datocms-assets.com/106424/1695806542-og_social.png'>
+      <RootLayout socialUmageUrl='https://www.datocms-assets.com/106424/1695809132-og_social.png'>
         <Component {...pageProps} />
       </RootLayout>
     </main>
