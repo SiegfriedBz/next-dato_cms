e895a5ce2fecf80b6f39fb2586854a5e6ef35197
siegfriedbz <siegfried.bozza@yahoo.com>
2023-09-27 12:15:38 +0200

    add og social im on cloudinaryg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,9 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return (
     <main className='min-h-screen dark:bg-slate-950 dark:text-white'>
-      <RootLayout socialUmageUrl='https://www.datocms-assets.com/106424/1695809132-og_social.png'>
+      <RootLayout
+        socialUmageUrl={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/w_1200,h_630/v1695809496/webwizzards/og_social_webwizzards`}
+      >
         <Component {...pageProps} />
       </RootLayout>
     </main>
